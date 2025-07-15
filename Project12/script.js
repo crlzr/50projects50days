cd0d4b895a1c032d4fbebbbace89cc0e14320ecb
Carole <c.zenruffinen@hotmail.com>
2025-07-15 11:48:46 +1000

    simplifying code

--- Project12/script.js
+++ Project12/script.js
@@ -2,9 +2,6 @@ const faqToggles = document.querySelectorAll('.faq-toggle');
 
 faqToggles.forEach((toggle) => {
     toggle.addEventListener('click', () => {
-        // Remove 'active' from all FAQs to show only one at a time
-        document.querySelectorAll('.faq').forEach(faq => faq.classList.remove('active'));
-        // Toggle 'active' on the clicked FAQ
         toggle.parentNode.classList.toggle('active');
     });
 });
