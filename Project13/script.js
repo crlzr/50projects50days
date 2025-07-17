716c72e246b4dbb255e9d29b0df6106f8e9ff392
Carole <c.zenruffinen@hotmail.com>
2025-07-17 17:17:56 +1000

    updating project

--- Project13/script.js
+++ Project13/script.js
@@ -7,11 +7,13 @@ textarea.focus()
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
 
-    if(e.key === "Enter") {
+    if (e.key === "Enter") {
         setTimeout(() => {
             e.target.value = ''
         }, 10)
-        randomSelect()
+        if (document.querySelectorAll('.tag').length > 0) {
+            randomSelect()
+        }
     }
 })
 
